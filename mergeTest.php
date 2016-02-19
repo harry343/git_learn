322e8888bdd0b8c0c08d425f69a18c074f72d00c
harry34 <isitharry@mail.com>
2016-02-19 11:52:39 +0530

    fix

--- mergeTest.php
+++ mergeTest.php
@@ -6,3 +6,4 @@ added mmore text in github.com
 **did an another fix*
 **did one more fix on 'fix-2' branch*
 **did one more fix on 'fix-3' branch*
+same thing will be write in two branchs to see how merge conflict works
